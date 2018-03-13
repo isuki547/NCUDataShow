b5a5895f6631464967cf06dbbd80d7a939fa7d43
suki547 <jlosuki@hotmail.com>
2018-03-13 14:30:50 +0800

    0313 appmodule错误

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { OverlayContainer } from '@angular/cdk/overlay';
+import {  trigger,state,transition,style,animate,keyframes} from "@angular/animations";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  
 })
 export class AppComponent {
-  title = 'app';
+  squareState:string;
+  darkTheme = false;
+
+ constructor(private oc:OverlayContainer){
+
+  }
+  switchTheme(dark){
+    this.darkTheme=dark;
+    this.oc.getContainerElement().className=dark ? 'myapp-dark-theme' : null;
+  }
+ 
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { MatSidenavModule } from "@angular/material";
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { CoreModule} from './core/core.module';
+import { LoginModule } from "./login/login.module";
+import { ProjectModule } from './project/project.module';
+import { BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { TaskModule } from './task/task.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+     BrowserModule, 
+     MatSidenavModule,
+     AppRoutingModule,
+     LoginModule,
+     CoreModule,
+     ProjectModule,
+     TaskModule,
+     BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
